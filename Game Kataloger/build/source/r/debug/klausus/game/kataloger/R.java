b37178278044ab45cc8c45a7dcdcdb38c625427c
Klausus <Klausus@Gmail.com>
2013-08-09 04:28:08 +0200

    Home/Start Layout concept created/made it. Pantalla de inicio/bienvenida creada.

--- Game Kataloger/build/source/r/debug/klausus/game/kataloger/R.java
+++ Game Kataloger/build/source/r/debug/klausus/game/kataloger/R.java
@@ -18,10 +18,19 @@ public final class R {
         public static final int activity_vertical_margin=0x7f040001;
     }
     public static final class drawable {
-        public static final int ic_launcher=0x7f020000;
+        public static final int add=0x7f020000;
+        public static final int games=0x7f020001;
+        public static final int ic_launcher=0x7f020002;
+        public static final int transparent=0x7f020003;
     }
     public static final class id {
-        public static final int action_settings=0x7f080000;
+        public static final int action_settings=0x7f080006;
+        public static final int imageButton1=0x7f080000;
+        public static final int imageButton2=0x7f080001;
+        public static final int imageButton3=0x7f080002;
+        public static final int imageButton4=0x7f080003;
+        public static final int imageButton5=0x7f080004;
+        public static final int imageButton6=0x7f080005;
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
@@ -33,8 +42,14 @@ public final class R {
         /**  From: file:/D:/GIT/GameKataloger/Game%20Kataloger/src/main/res/values/strings.xml 
          */
         public static final int action_settings=0x7f050000;
-        public static final int app_name=0x7f050001;
-        public static final int hello_world=0x7f050002;
+        public static final int addgame=0x7f050001;
+        public static final int app_name=0x7f050002;
+        public static final int consoles=0x7f050003;
+        public static final int games=0x7f050004;
+        public static final int genres=0x7f050005;
+        public static final int hello_world=0x7f050006;
+        public static final int plataform=0x7f050007;
+        public static final int wishlist=0x7f050008;
     }
     public static final class style {
         /**  From: file:/D:/GIT/GameKataloger/Game%20Kataloger/src/main/res/values/styles.xml 
